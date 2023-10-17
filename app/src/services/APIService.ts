21c397c0407d564a9de9c96bc947802355b68dc2
Andy Wu <andyctct.wu@mail.utoronto.ca>
2023-10-17 18:42:05 -0400

    Undo incorrect removal of APIService refactoring

--- app/src/services/APIService.ts
+++ app/src/services/APIService.ts
@@ -3,40 +3,7 @@ import base64 from "react-native-base64";
 import { DeviceId } from "react-native-ble-plx";
 import { DBService } from "./DBService";
 
-const adminUrl =
-    "https://4aerjx42n3.execute-api.ca-central-1.amazonaws.com/dev/admin";
-const userUrl =
-    "https://4aerjx42n3.execute-api.ca-central-1.amazonaws.com/dev/users";
-
-// const adminUrl = "http://172.26.204.194:4100/admin";
-// const userUrl = "http://172.26.204.194:4000/users";
-
-const AmplifyConfig = {
-    Auth: {
-        region: "ca-central-1",
-        userPoolId: "ca-central-1_WoRPGIWId",
-        userPoolWebClientId: "26jitbu3pli31avgpomggu7gvt",
-    },
-    API: {
-        endpoints: [
-            {
-                name: "AdminBackend",
-                endpoint: adminUrl,
-                custom_header: async () => {
-                    const token = (await Auth.currentSession())
-                        .getIdToken()
-                        .getJwtToken();
-                    return { Authorization: `Bearer ${token}` };
-                },
-            },
-            {
-                name: "UserBackend",
-                endpoint: userUrl,
-            },
-        ],
-    },
-};
-
+const localhost = "192.168.88.156"
 
 class APIServiceInstance {
     apiKey: string | null = null;
@@ -65,45 +32,34 @@ class APIServiceInstance {
 
     syncToCloudForDevice = async (deviceId: string) => {
         console.log("syncing to cloud", deviceId);
-        const cloudSyncInfo = await DBService.getCloudSyncInfoForDevice(deviceId);
-        const readings = await DBService.getReadings(deviceId, cloudSyncInfo.lastSyncedId);
-        const messages = readings.map((reading) => reading.message);
+        try {
+            const cloudSyncInfo = await DBService.getCloudSyncInfoForDevice(deviceId);
+            const readings = await DBService.getReadings(deviceId, cloudSyncInfo.last_synced_id);
 
-        // API.post("UserBackend", "/readings", {
-        //     body: readings.map((r) => ({
-        //         synced: r.synced,
-        //         message: r.message,
-        //     })),
-        //     queryStringParameters: {
-        //         apiKey: this.apiKey,
-        //     },
-        // })
-        //     .then(({ interval }) => {
-        //     })
-        //     .catch((e) => {
-        //         console.log("failed to sync", e);
-        //     });
-        const response = fetch('https://localhost:3000/users/readings', {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-                'x-api-key': cloudSyncInfo.apiKey,
-            },
-            body: JSON.stringify({
-                deviceId: deviceId,
-                messages: messages
-            }),
-        }).then(response => response.json())
-            .then(json => {
-                console.log(json);
-                DBService.updateCloudSyncInfoForDevice(deviceId, readings[readings.length - 1].id, cloudSyncInfo.apiKey);
-                return json;
-            })
-            .catch(error => {
-                console.error(error);
-            });
-        console.log("synced to cloud", response);
+            const response = fetch('http://' + localhost + ':3000/users/readings', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    'x-api-key': cloudSyncInfo.api_key,
+                },
+                body: JSON.stringify({
+                    deviceId: deviceId,
+                    messages: readings
+                }),
+            }).then(response => {
+                    return response;
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+
+            await DBService.updateCloudSyncInfoForDevice(deviceId, readings[readings.length - 1].id, cloudSyncInfo.api_key);
+            console.log("synced to cloud", response);
+        } catch (e) {
+            console.log("failed to sync", e);
+            return;
+        }
     }
 
     registerDevice = async (
@@ -117,39 +73,9 @@ class APIServiceInstance {
         }
         console.log("id", hexId);
 
-        // try {
-        //     const response = await API.post("AdminBackend", "/register-device", {
-        //         body: { deviceId: hexId },
-        //     });
-        //     console.log("Response", response);
-        //     apiKey = response;
-        // } catch (e) {
-        //     console.log("sending failed", { deviceId: hexId }, e);
-        //     throw new Error("Failed to register with backend");
-        // }
-        const apiKey = fetch('https://localhost:3000/users/register', {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                deviceId: hexId
-            }),
-        })
-            .then(response => response.json())
-            .then(json => {
-                DBService.insertCloudSyncInfoForDevice(hexId, 0, json.apiKey);
-                this.apiKey = json.apiKey;
-                return json.apiKey;
-            })
-            .catch(error => {
-                console.error(error);
-=======
         try {
             const response = await API.post("AdminBackend", "/register-device", {
                 body: { deviceId: hexId },
->>>>>>> Stashed changes
             });
             console.log("Response", response);
             const apiKey = response;
