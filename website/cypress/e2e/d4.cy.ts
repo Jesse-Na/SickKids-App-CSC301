9dd815244305123efcfe72908575c9c77ced5b86
Eddy <m1662576219@gmail.com>
2023-11-29 00:23:31 -0500

    sk-4 testings

--- website/cypress/e2e/d4.cy.ts
+++ website/cypress/e2e/d4.cy.ts
@@ -1,26 +1,28 @@
-
 describe("My First Cypress Test", () => {
   it("Visits the website and test frequency change", () => {
-
     // Visit the website
     cy.visit("https://test-7a901.web.app/device/1FFF7590");
-    
+
     // click sign out
-    cy.get('[style="flex-grow: 1; display: flex; justify-content: flex-end; padding-right: 10px;"] > div > .MuiButtonBase-root').click();
-    cy.get('.MuiPaper-root > .MuiButtonBase-root').click();
-    
+    cy.get(
+      '[style="flex-grow: 1; display: flex; justify-content: flex-end; padding-right: 10px;"] > div > .MuiButtonBase-root'
+    ).click();
+    cy.get(".MuiPaper-root > .MuiButtonBase-root").click();
+
     // sign in with credentials
-    cy.get('#\\:r0\\:').type('yicong.luo@mail.utoronto.ca');
-    cy.get('#\\:r1\\:').type('qynbo6-wAfbuz-hucmow');
+    cy.get("#\\:r0\\:").type("yicong.luo@mail.utoronto.ca");
+    cy.get("#\\:r1\\:").type("qynbo6-wAfbuz-hucmow");
 
     // click sign in
-    cy.get(':nth-child(4) > .MuiButtonBase-root').click();
-    
+    cy.get(":nth-child(4) > .MuiButtonBase-root").click();
+
     // click device button
-    cy.get('[style="display: flex; padding-left: 60px;"] > :nth-child(1)').click();
+    cy.get(
+      '[style="display: flex; padding-left: 60px;"] > :nth-child(1)'
+    ).click();
 
     // click first device
-    cy.get('.MuiTableBody-root > :nth-child(1) > :nth-child(1)').click();
+    cy.get(".MuiTableBody-root > :nth-child(1) > :nth-child(1)").click();
 
     // if acknowledge button exists, click it
     cy.get("body").then(($body) => {
@@ -36,13 +38,5 @@ describe("My First Cypress Test", () => {
       }
     });
     
-    // click edit button
-    cy.get('.App > :nth-child(2) > :nth-child(2)').click();
-    
-    // change frequency
-    cy.get('#\\:r1n\\:').type("1");
-
-    // click save button
-    cy.get('.MuiDialogActions-root > :nth-child(2)').click();
   });
 });
