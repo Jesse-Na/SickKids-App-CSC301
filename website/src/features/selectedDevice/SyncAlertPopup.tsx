7d064882bc1810e1338364170dfbc4c2f52ef14e
Eddy <m1662576219@gmail.com>
2023-11-29 00:23:09 -0500

    button position:

--- website/src/features/selectedDevice/SyncAlertPopup.tsx
+++ website/src/features/selectedDevice/SyncAlertPopup.tsx
@@ -1,35 +1,39 @@
 import {
-    Button,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogTitle,
-    Typography,
-  } from "@mui/material";
-  import React from "react";
-  import { Device } from "../../utils/types";
-  
-  type Props = {
-    open: boolean;
-    handleClose: () => void;
-    device: Device;
-  };
-  
-  export default function SyncAlertPopup({ open, handleClose, device }: Props) {
-    return (
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Sync Alert</DialogTitle>
-        <DialogContent>
-          <Typography>
-            The device '{device.name}' has not been synced for over 24 hours.
-          </Typography>
-        </DialogContent>
-        <DialogActions>
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Typography,
+  Box
+} from "@mui/material";
+import React from "react";
+import { Device } from "../../utils/types";
+
+type Props = {
+  open: boolean;
+  handleClose: () => void;
+  device: Device;
+};
+
+export default function SyncAlertPopup({ open, handleClose, device }: Props) {
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle>Sync Alert</DialogTitle>
+      <DialogContent>
+        <Typography>
+          The device '{device.name}' has not been synced for over 24 hours.
+        </Typography>
+      </DialogContent>
+      <DialogActions>
+        <Box display="flex" justifyContent="center" width="100%"> {/* Use Box to create a flex container */}
           <Button variant="contained" onClick={handleClose}>
             Acknowledge
           </Button>
-        </DialogActions>
-      </Dialog>
-    );
-  }
+        </Box>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
   
